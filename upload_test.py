9d3af9c4c5506a82a5c3a019d79630fffec33268
yoorhee <euria109@yonsei.ac.kr>
2025-07-16 22:00:54 +0900

    Modify: 2nd trial

--- upload_test.py
+++ upload_test.py
@@ -1,4 +1,5 @@
 print("GitHub Actions is running the upload_test.py script.")
 
 print('If I change this file, it will trigger a GitHub Actions workflow.')
-print('This is a test to ensure that the upload_test.py script is functioning correctly.')
+print('I want to check if this change will be detected by GitHub Actions.')
+print("Here's a test to ensure that the upload_test.py script is functioning correctly.")

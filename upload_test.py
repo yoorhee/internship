aa13bd96208b5c04efd8b3e15cf3577dfa382fc4
yoorhee <euria109@yonsei.ac.kr>
2025-07-16 21:57:48 +0900

    Add: New print sentence

--- upload_test.py
+++ upload_test.py
@@ -1,1 +1,4 @@
 print("GitHub Actions is running the upload_test.py script.")
+
+print('If I change this file, it will trigger a GitHub Actions workflow.')
+print('This is a test to ensure that the upload_test.py script is functioning correctly.')
